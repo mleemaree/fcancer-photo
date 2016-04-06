61c6d276b7c107b55d22f598355135b43e4d7f1d
lee <lee@mleemaree.com>
2016-04-06 08:18:36 +0200

    slider

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($){
 
   var s = skrollr.init();
-  var titles = ['Page 1', 'Page 2', 'Page 3'];
+  var titles = ['Slide 1', 'Slide 2', 'Slide 3'];
   var mySwiper = new Swiper ('.swiper-container', {
       // Optional parameters
       direction: 'vertical',
@@ -25,20 +25,20 @@ jQuery(document).ready(function($){
   }
 
   function closeChat(){
-    $('#lp').removeClass('hide');
-    $('#chat').addClass('hide');
+    // $('#lp').removeClass('hide');
+    // $('#chat').addClass('hide');
     $('body').removeClass('x');
     $('#br').removeClass('shut');
   }
 
   $('.inv').on('click', function(){
     $('#br').addClass('shut');
-    openChat();
+    setTimeout(openChat, 500);
   });
 
   $('#back').on('click', function(){
     $('#br').addClass('shut');
-    closeChat();
+    setTimeout(closeChat, 10);
   });
 
 });
